31c910265edb7b2c395eb3100addae0e85730761
League <League@administorsimac.attlocal.net>
2018-02-17 16:42:32 -0800

    The Riddler Secret Message Box 2/17/18

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,24 +1,26 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		String score;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String answer = JOptionPane.showInputDialog("How many months have 28 days?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (answer.equals("12") || answer.equals("All of them")) { JOptionPane.showMessageDialog(null, "Correct!"); }
+			// 5. Otherwise, say "wrong" and tell them the answer
+		else {
+			JOptionPane.showMessageDialog(null, "WRONG!");
+		}
+			// 6. Add some more riddles
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+			// 2. Make a pop up to show the score.
 
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
-		
+		}
 	}
-}
-
